a75cca3aecd33397e1e6491a94247911246fcde9
roralo <roberto.rajo.lopez@gmail.com>
2025-11-27 14:20:30 +0100

    Use H2 profile for Spring Boot context test

--- src/test/java/com/portfolio/users/UsersServiceApplicationTests.java
+++ src/test/java/com/portfolio/users/UsersServiceApplicationTests.java
@@ -2,11 +2,13 @@ package com.portfolio.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UsersServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 }
